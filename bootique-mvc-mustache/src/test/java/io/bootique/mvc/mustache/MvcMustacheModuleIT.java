509d3c2626c1191fca3d555b351f98111922cfb7
Andrus Adamchik <andrus@objectstyle.com>
2016-09-18 14:59:00 -0400

    upgrading unit tests to BQ #97

--- bootique-mvc-mustache/src/test/java/io/bootique/mvc/mustache/MvcMustacheModuleIT.java
+++ bootique-mvc-mustache/src/test/java/io/bootique/mvc/mustache/MvcMustacheModuleIT.java
@@ -1,6 +1,5 @@
 package io.bootique.mvc.mustache;
 
-import io.bootique.Bootique;
 import io.bootique.jersey.JerseyModule;
 import io.bootique.jetty.test.junit.JettyTestFactory;
 import io.bootique.mvc.mustache.view.ConcreteView;
@@ -16,80 +15,80 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import java.util.function.Consumer;
 
 import static org.junit.Assert.assertEquals;
 
 public class MvcMustacheModuleIT {
 
-	@ClassRule
-	public static JettyTestFactory TEST_SERVER = new JettyTestFactory();
+    @ClassRule
+    public static JettyTestFactory TEST_SERVER = new JettyTestFactory();
 
-	@BeforeClass
-	public static void beforeClass() {
+    @BeforeClass
+    public static void beforeClass() {
+        TEST_SERVER.app()
+                .args("--config=classpath:MvcMustacheModuleIT.yml")
+                .autoLoadModules()
+                .module(binder -> JerseyModule.contributeResources(binder).addBinding().to(Api.class))
+                .startServer();
+    }
 
-		Consumer<Bootique> configurator = bq -> bq.args("--config=classpath:MvcMustacheModuleIT.yml").autoLoadModules()
-				.module(binder -> JerseyModule.contributeResources(binder).addBinding().to(Api.class));
-		TEST_SERVER.newRuntime().configurator(configurator).startServer();
-	}
+    @Test
+    public void testV1() {
+        WebTarget base = ClientBuilder.newClient().target("http://localhost:8080");
+        Response r1 = base.path("/v1").request().get();
+        assertEquals(Status.OK.getStatusCode(), r1.getStatus());
+        assertEquals("v1_string_p1_number_564", r1.readEntity(String.class));
+    }
 
-	@Test
-	public void testV1() {
-		WebTarget base = ClientBuilder.newClient().target("http://localhost:8080");
-		Response r1 = base.path("/v1").request().get();
-		assertEquals(Status.OK.getStatusCode(), r1.getStatus());
-		assertEquals("v1_string_p1_number_564", r1.readEntity(String.class));
-	}
+    @Test
+    public void testV2() {
+        WebTarget base = ClientBuilder.newClient().target("http://localhost:8080");
+        Response r1 = base.path("/v2").request().get();
+        assertEquals(Status.OK.getStatusCode(), r1.getStatus());
+        assertEquals("v2_string_p2_number_5649", r1.readEntity(String.class));
+    }
 
-	@Test
-	public void testV2() {
-		WebTarget base = ClientBuilder.newClient().target("http://localhost:8080");
-		Response r1 = base.path("/v2").request().get();
-		assertEquals(Status.OK.getStatusCode(), r1.getStatus());
-		assertEquals("v2_string_p2_number_5649", r1.readEntity(String.class));
-	}
+    @Path("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public static class Api {
 
-	@Path("/")
-	@Produces(MediaType.TEXT_PLAIN)
-	public static class Api {
+        @GET
+        @Path("/v1")
+        public ConcreteView getV1() {
+            Model m = new Model();
+            m.setProp1("p1");
+            m.setProp2(564);
+            return new ConcreteView("MvcMustacheModuleIT_v1.mustache", m);
+        }
 
-		@GET
-		@Path("/v1")
-		public ConcreteView getV1() {
-			Model m = new Model();
-			m.setProp1("p1");
-			m.setProp2(564);
-			return new ConcreteView("MvcMustacheModuleIT_v1.mustache", m);
-		}
+        @GET
+        @Path("/v2")
+        public ConcreteView getV2() {
+            Model m = new Model();
+            m.setProp1("p2");
+            m.setProp2(5649);
+            return new ConcreteView("MvcMustacheModuleIT_v2.mustache", m);
+        }
+    }
 
-		@GET
-		@Path("/v2")
-		public ConcreteView getV2() {
-			Model m = new Model();
-			m.setProp1("p2");
-			m.setProp2(5649);
-			return new ConcreteView("MvcMustacheModuleIT_v2.mustache", m);
-		}
-	}
+    public static class Model {
+        private String prop1;
+        private int prop2;
 
-	public static class Model {
-		private String prop1;
-		private int prop2;
+        public String getProp1() {
+            return prop1;
+        }
 
-		public String getProp1() {
-			return prop1;
-		}
+        public void setProp1(String prop1) {
+            this.prop1 = prop1;
+        }
 
-		public int getProp2() {
-			return prop2;
-		}
+        public int getProp2() {
+            return prop2;
+        }
 
-		public void setProp1(String prop1) {
-			this.prop1 = prop1;
-		}
-
-		public void setProp2(int prop2) {
-			this.prop2 = prop2;
-		}
-	}
+        public void setProp2(int prop2) {
+            this.prop2 = prop2;
+        }
+    }
 }
